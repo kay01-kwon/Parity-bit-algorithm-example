3bdcacd418b515a63b1728a34d2af6d24a2e5b93
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2023-07-05 01:28:27 +0900

    Add files via upload

--- even_parity/src/main.cpp
+++ even_parity/src/main.cpp
@@ -5,8 +5,13 @@ int main()
     bool check_parity;
     even_parity test;
 
-    test.set_command(0x0404);
+    test.set_command(0xff04);
     test.print_binary();
+
+    test.bit_modify(13,0);
+    test.bit_modify(12,0);
+    test.print_binary();
+
 
     check_parity = test.is_even_parity();
 
